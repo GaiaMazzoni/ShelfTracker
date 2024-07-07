fa23baae99a841cf1c59a18726a30936fe98df6f
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2024-07-07 14:35:25 +0200

    fixes

--- app/src/main/java/com/example/shelftracker/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shelftracker/ui/screens/home/HomeScreen.kt
@@ -273,9 +273,8 @@ fun BookItem(item: Book, booksVm: BooksViewModel, onClick: () -> Unit) {
                 ImageWithPlaceholder(imageUri, Size.Sm)
             }
             Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .width(LocalConfiguration.current.screenWidthDp.dp / 3 + 36.dp)
+                modifier = Modifier.padding(0.dp, 16.dp)
+                    .width(LocalConfiguration.current.screenWidthDp.dp / 3 + LocalConfiguration.current.screenWidthDp.dp / 5)
             ) {
                 Text(
                     item.title,
